e4ec1b99750075276bb43fdf3260443c217e5bfa
lucasdarini <lucas.darini@inovafarma.com.br>
2023-02-16 16:07:45 -0300

    feat(DetailsView): add loading, and more widgets

--- lib/ios/views/details.view.dart
+++ lib/ios/views/details.view.dart
@@ -1,8 +1,12 @@
 import 'package:contacts/ios/views/address.view.dart';
-import 'package:contacts/ios/styles.dart';
 import 'package:contacts/ios/views/editor-contact.view.dart';
+import 'package:contacts/ios/views/loading.view.dart';
 import 'package:contacts/models/contact.model.dart';
+import 'package:contacts/repositories/contact.repository.dart';
+import 'package:contacts/shared/widgets/contact-details-description.widget.dart';
+import 'package:contacts/shared/widgets/contact-details-image.widget.dart';
 import 'package:flutter/cupertino.dart';
+import 'package:url_launcher/url_launcher.dart';
 
 class DetailsView extends StatefulWidget {
   final int id;
@@ -13,173 +17,162 @@ class DetailsView extends StatefulWidget {
 }
 
 class _DetailsViewState extends State<DetailsView> {
+  final _repository = ContactRepository();
+
   @override
   Widget build(BuildContext context) {
-    return CupertinoPageScaffold(
-      child: CustomScrollView(
-        slivers: <Widget>[
-          CupertinoSliverNavigationBar(
-            largeTitle: const Text(
-              "Contato",
-            ),
-            trailing: CupertinoButton(
-              child: const Icon(
-                CupertinoIcons.pen,
-              ),
-              onPressed: () {
-                Navigator.push(
-                  context,
-                  CupertinoPageRoute(
-                    builder: (context) => EditorContactView(
-                      contactModel: ContactModel(
-                        id: 1,
-                        name: "Lucas Darini",
-                        email: "lucas.rocha15@fatec.sp.gov.br",
-                        phone: "00 99999-9999",
-                        addressLine1: 'Rua desenvolvedor',
-                        addressLine2: 'Nº 0',
-                        latLng: '',
-                      ),
-                    ),
-                  ),
-                );
-              },
-            ),
-          ),
-          SliverFillRemaining(
-            child: Column(
-              children: <Widget>[
-                const SizedBox(
-                  height: 10,
-                  width: double.infinity,
-                ),
-                Container(
-                  padding: const EdgeInsets.all(10),
-                  width: 200,
-                  height: 200,
-                  decoration: BoxDecoration(
-                    color: primaryColor.withOpacity(0.1),
-                    borderRadius: BorderRadius.circular(200),
-                  ),
-                  child: Container(
-                    width: 100,
-                    height: 100,
-                    decoration: BoxDecoration(
-                      image: const DecorationImage(
-                        image: NetworkImage("https://place-hold.it/200"),
-                      ),
-                      borderRadius: BorderRadius.circular(100),
-                    ),
+    return FutureBuilder(
+      future: _repository.getContact(widget.id),
+      builder: (context, snapshot) {
+        if (snapshot.hasData) {
+          ContactModel contactModel = snapshot.data!;
+          return CupertinoPageScaffold(
+            child: CustomScrollView(
+              slivers: <Widget>[
+                CupertinoSliverNavigationBar(
+                  largeTitle: const Text('Contato'),
+                  trailing: GestureDetector(
+                    child: const Icon(CupertinoIcons.pen),
+                    onTap: () {
+                      Navigator.push(
+                        context,
+                        CupertinoPageRoute(
+                          builder: (_) =>
+                              EditorContactView(contactModel: contactModel),
+                        ),
+                      );
+                    },
                   ),
                 ),
-                const SizedBox(
-                  height: 10,
-                ),
-                const Text(
-                  "Lucas Darini",
-                  style: TextStyle(
-                    fontSize: 18,
-                    fontWeight: FontWeight.bold,
-                  ),
-                ),
-                const Text(
-                  "00 99999-9999",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                const Text(
-                  "lucas.rocha15@fatec.sp.gov.br",
-                  style: TextStyle(
-                    fontSize: 14,
-                    fontWeight: FontWeight.w400,
-                  ),
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Row(
-                  mainAxisAlignment: MainAxisAlignment.spaceAround,
-                  children: <Widget>[
-                    CupertinoButton(
-                      onPressed: () {},
-                      child: const Icon(
-                        CupertinoIcons.phone,
-                      ),
-                    ),
-                    CupertinoButton(
-                      onPressed: () {},
-                      child: const Icon(
-                        CupertinoIcons.mail,
-                      ),
-                    ),
-                    CupertinoButton(
-                      onPressed: () {},
-                      child: const Icon(
-                        CupertinoIcons.photo_camera,
-                      ),
-                    ),
-                  ],
-                ),
-                const SizedBox(
-                  height: 20,
-                ),
-                Padding(
-                  padding: const EdgeInsets.all(20.0),
-                  child: Row(
+                SliverFillRemaining(
+                  child: Column(
                     children: <Widget>[
-                      Expanded(
-                        child: Column(
-                          mainAxisAlignment: MainAxisAlignment.start,
-                          crossAxisAlignment: CrossAxisAlignment.start,
-                          children: const <Widget>[
-                            SizedBox(
-                              width: double.infinity,
+                      const SizedBox(
+                        height: 10,
+                        width: double.infinity,
+                      ),
+                      ContactDetailsImage(
+                        image: contactModel.image,
+                      ),
+                      const SizedBox(
+                        height: 10,
+                      ),
+                      ContactDetailsDescription(
+                        name: contactModel.name!,
+                        phone: contactModel.phone!,
+                        email: contactModel.email!,
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Row(
+                        mainAxisAlignment: MainAxisAlignment.spaceAround,
+                        children: <Widget>[
+                          CupertinoButton(
+                            child: const Icon(
+                              CupertinoIcons.phone,
                             ),
-                            Text(
-                              "Endereço",
-                              style: TextStyle(
-                                fontWeight: FontWeight.w600,
-                                fontSize: 16,
+                            onPressed: () {
+                              final Uri _uri = Uri.parse(
+                                'tel:${contactModel.phone}'
+                                    .replaceAll('(', '')
+                                    .replaceAll(')', '')
+                                    .replaceAll('-', ''),
+                              );
+                              launchUrl(_uri);
+                            },
+                          ),
+                          CupertinoButton(
+                            child: const Icon(
+                              CupertinoIcons.mail,
+                            ),
+                            onPressed: () {
+                              final Uri uri =
+                                  Uri.parse('mailto:${contactModel.email}');
+                              launchUrl(uri);
+                            },
+                          ),
+                          CupertinoButton(
+                            child: const Icon(
+                              CupertinoIcons.photo_camera,
+                            ),
+                            onPressed: () {},
+                          ),
+                        ],
+                      ),
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      Padding(
+                        padding: const EdgeInsets.all(20.0),
+                        child: Row(
+                          children: [
+                            Expanded(
+                              child: Column(
+                                mainAxisAlignment: MainAxisAlignment.start,
+                                crossAxisAlignment: CrossAxisAlignment.start,
+                                children: <Widget>[
+                                  const SizedBox(
+                                    width: double.infinity,
+                                  ),
+                                  const Text(
+                                    "Endereço",
+                                    style: TextStyle(
+                                      fontWeight: FontWeight.w600,
+                                      fontSize: 16,
+                                    ),
+                                  ),
+                                  Text(
+                                    contactModel.addressLine1 ??
+                                        'Nenhum endereço cadastrado',
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                  Text(
+                                    contactModel.addressLine2 ?? '',
+                                    style: const TextStyle(
+                                      fontSize: 12,
+                                    ),
+                                  ),
+                                ],
                               ),
                             ),
-                            Text(
-                              "Rua do Desenvolvedor, 256",
-                              style: TextStyle(
-                                fontSize: 12,
-                              ),
-                            ),
-                            Text(
-                              "Piracicaba/SP",
-                              style: TextStyle(
-                                fontSize: 12,
+                            CupertinoButton(
+                              onPressed: () {
+                                Navigator.push(
+                                  context,
+                                  CupertinoPageRoute(
+                                    builder: (context) => const AddressView(),
+                                  ),
+                                );
+                              },
+                              child: const Icon(
+                                CupertinoIcons.location,
                               ),
                             ),
                           ],
                         ),
                       ),
-                      CupertinoButton(
-                        onPressed: () {
-                          Navigator.push(
-                            context,
-                            CupertinoPageRoute(
-                              builder: (context) => const AddressView(),
-                            ),
-                          );
-                        },
-                        child: const Icon(
-                          CupertinoIcons.location,
+                      const SizedBox(
+                        height: 20,
+                      ),
+                      CupertinoButton.filled(
+                        child: const Text(
+                          'Excluir Contato',
                         ),
+                        onPressed: () {},
                       ),
                     ],
                   ),
                 ),
               ],
             ),
-          ),
-        ],
-      ),
+          );
+        } else {
+          return const LoadginView();
+        }
+      },
     );
   }
 }
