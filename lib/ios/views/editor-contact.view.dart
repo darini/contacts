d4062c0c7406189d45b08115166ee98a6d492093
lucasdarini <lucas.darini@inovafarma.com.br>
2023-02-16 14:32:35 -0300

    feat(EditorContactView): add create/update functions

--- lib/ios/views/editor-contact.view.dart
+++ lib/ios/views/editor-contact.view.dart
@@ -1,10 +1,93 @@
+import 'package:contacts/ios/views/home.view.dart';
 import 'package:contacts/models/contact.model.dart';
+import 'package:contacts/repositories/contact.repository.dart';
 import 'package:flutter/cupertino.dart';
 
-class EditorContactView extends StatelessWidget {
-  final ContactModel? model;
+class EditorContactView extends StatefulWidget {
+  final ContactModel contactModel;
 
-  const EditorContactView({Key? key, required this.model}) : super(key: key);
+  const EditorContactView({Key? key, required this.contactModel})
+      : super(key: key);
+
+  @override
+  State<EditorContactView> createState() => _EditorContactViewState();
+}
+
+class _EditorContactViewState extends State<EditorContactView> {
+  final _formKey = GlobalKey<FormState>();
+  final _repository = ContactRepository();
+
+  onSubmit() {
+    if (!_formKey.currentState!.validate()) {
+      return;
+    }
+
+    _formKey.currentState!.save();
+
+    if (widget.contactModel.id == 0) {
+      create();
+    } else {
+      update();
+    }
+  }
+
+  create() {
+    _repository
+        .create(widget.contactModel)
+        .then(
+          (_) => {
+            onSuccess(),
+          },
+        )
+        .onError(
+          (error, stackTrace) => {
+            onError(),
+          },
+        );
+  }
+
+  update() {
+    _repository
+        .update(widget.contactModel)
+        .then(
+          (_) => {
+            onSuccess(),
+          },
+        )
+        .onError(
+          (error, stackTrace) => {
+            onError(),
+          },
+        );
+  }
+
+  onSuccess() {
+    Navigator.push(
+      context,
+      CupertinoPageRoute(
+        builder: (context) => const HomeView(),
+      ),
+    );
+  }
+
+  onError() {
+    showCupertinoDialog(
+      context: context,
+      builder: (context) {
+        return CupertinoAlertDialog(
+          title: const Text('Falha na operação'),
+          content: const Text('Ops, algo deu erro'),
+          actions: [
+            CupertinoButton(
+                child: const Text('OK'),
+                onPressed: () {
+                  Navigator.pop(context);
+                }),
+          ],
+        );
+      },
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -12,7 +95,7 @@ class EditorContactView extends StatelessWidget {
       child: CustomScrollView(
         slivers: <Widget>[
           CupertinoSliverNavigationBar(
-            largeTitle: model == null
+            largeTitle: widget.contactModel.id == 0
                 ? const Text("Novo Contato")
                 : const Text("Editar Contato"),
           ),
@@ -20,22 +103,38 @@ class EditorContactView extends StatelessWidget {
             child: Padding(
               padding: const EdgeInsets.all(20),
               child: Form(
+                key: _formKey,
                 child: Column(
                   children: <Widget>[
                     CupertinoTextField(
-                      placeholder: model?.name ?? "Nome",
+                      placeholder: widget.contactModel.name,
+                      textCapitalization: TextCapitalization.words,
+                      keyboardType: TextInputType.text,
+                      onChanged: (val) {
+                        widget.contactModel.name = val;
+                      },
                     ),
                     const SizedBox(
                       height: 20,
                     ),
                     CupertinoTextField(
-                      placeholder: model?.phone ?? "Telefone",
+                      placeholder: widget.contactModel.phone,
+                      textCapitalization: TextCapitalization.words,
+                      keyboardType: TextInputType.phone,
+                      onChanged: (val) {
+                        widget.contactModel.phone = val;
+                      },
                     ),
                     const SizedBox(
                       height: 20,
                     ),
                     CupertinoTextField(
-                      placeholder: model?.email ?? "E-mail",
+                      placeholder: widget.contactModel.email,
+                      textCapitalization: TextCapitalization.words,
+                      keyboardType: TextInputType.emailAddress,
+                      onChanged: (val) {
+                        widget.contactModel.email = val;
+                      },
                     ),
                     const SizedBox(
                       height: 20,
