39062fd52593005651c64b5d589d3855e352e501
lucasdarini <lucas.darini@inovafarma.com.br>
2023-02-20 15:13:02 -0300

    feat(SplashView): localAuthenticationService config

--- lib/android/android.app.dart
+++ lib/android/android.app.dart
@@ -1,16 +1,19 @@
+import 'package:contacts/android/services/local-authentication.service.dart';
 import 'package:contacts/android/styles.dart';
-import 'package:contacts/android/views/home.view.dart';
+import 'package:contacts/android/views/splash.view.dart';
 import 'package:flutter/material.dart';
 
 class AndroidApp extends StatelessWidget {
   const AndroidApp({Key? key}) : super(key: key);
   @override
   Widget build(BuildContext context) {
+    final localAuthService = LocalAuthenticationService();
+
     return MaterialApp(
       title: 'Contacts',
       debugShowCheckedModeBanner: false,
       theme: androidTheme(),
-      home: const HomeView(),
+      home: SplashView(localAuthenticationService: localAuthService),
     );
   }
 }
--- lib/android/views/splash.view.dart
+++ lib/android/views/splash.view.dart
@@ -1,7 +1,36 @@
+import 'package:contacts/android/services/ilocal-auth.service.dart';
+import 'package:contacts/android/views/home.view.dart';
 import 'package:flutter/material.dart';
 
-class SplashView extends StatelessWidget {
-  const SplashView({Key? key}) : super(key: key);
+class SplashView extends StatefulWidget {
+  final ILocalAuthService localAuthenticationService;
+  const SplashView({Key? key, required this.localAuthenticationService})
+      : super(key: key);
+
+  @override
+  State<SplashView> createState() => _SplashViewState();
+}
+
+class _SplashViewState extends State<SplashView> {
+  @override
+  initState() {
+    super.initState();
+    Future.delayed(
+      const Duration(
+        seconds: 2,
+      ),
+    ).then((_) {
+      widget.localAuthenticationService.authenticate().then((result) {
+        if (result) {
+          Navigator.of(context).push(
+            MaterialPageRoute(
+              builder: (_) => const HomeView(),
+            ),
+          );
+        }
+      }).catchError((onError) {});
+    });
+  }
 
   @override
   Widget build(BuildContext context) {
