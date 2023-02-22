dd1a3eddf69be0d69f8e7d40736d5a21afca7d87
lucasdarini <lucas.darini@inovafarma.com.br>
2023-02-22 17:08:40 -0300

    feat(Settings): add MAPS_API_KEY (change in prod.)

--- lib/settings.dart
+++ lib/settings.dart
@@ -3,3 +3,5 @@ const String DATABASE_NAME = 'contacts.db';
 const String TABLE_NAME = 'contacts';
 const String CREATE_CONTACTS_TABLE_SCRIPT =
     'CREATE TABLE contacts(id INTEGER PRIMARY KEY,name TEXT,email TEXT,phone TEXT,image TEXT,addressLine1 TEXT,addressLine2 TEXT,latLng TEXT);';
+
+const String MAPS_API_KEY = 'xxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxxx';
