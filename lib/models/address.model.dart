e90b0d871d5c63ab5329d8b401cb3db0634bb126
lucasdarini <lucas.darini@inovafarma.com.br>
2023-02-22 17:02:04 -0300

    feat: add new AddressModel class

---
+++ lib/models/address.model.dart
@@ -0,0 +1,13 @@
+class AddressModel {
+  String longName;
+  String formattedAddress;
+  double latitude;
+  double longitude;
+
+  AddressModel({
+    required this.longName,
+    required this.formattedAddress,
+    required this.latitude,
+    required this.longitude,
+  });
+}
