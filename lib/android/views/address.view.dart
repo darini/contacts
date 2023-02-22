cdd47d03d34a264b5d2e78b137dcb9834781dfde
lucasdarini <lucas.darini@inovafarma.com.br>
2023-02-22 17:03:58 -0300

    feat(AddressView): implementing Google Maps functions

--- lib/android/views/address.view.dart
+++ lib/android/views/address.view.dart
@@ -1,7 +1,126 @@
+import 'package:contacts/models/contact.model.dart';
+import 'package:contacts/repositories/address.repository.dart';
 import 'package:flutter/material.dart';
+import 'package:geolocator/geolocator.dart';
+import 'package:google_maps_flutter/google_maps_flutter.dart';
+import 'package:uuid/uuid.dart';
 
-class AddressView extends StatelessWidget {
-  const AddressView({Key? key}) : super(key: key);
+class AddressView extends StatefulWidget {
+  final ContactModel contactModel;
+  const AddressView({Key? key, required this.contactModel}) : super(key: key);
+
+  @override
+  State<AddressView> createState() => _AddressViewState();
+}
+
+class _AddressViewState extends State<AddressView> {
+  final AddressRepository _addressRepository = AddressRepository();
+
+  late final GoogleMapController mapController;
+  LatLng _coordinates = const LatLng(45.521563, -122.677433);
+  Set<Marker>? markers;
+
+  @override
+  initState() {
+    super.initState();
+    if (widget.contactModel.latLng != null &&
+        widget.contactModel.latLng != "") {
+      var values = widget.contactModel.latLng?.split(',');
+
+      _coordinates = LatLng(
+        double.parse(values![0]),
+        double.parse(values[1]),
+      );
+    }
+  }
+
+  _onMapCreated(GoogleMapController controller) {
+    mapController = controller;
+  }
+
+  onSearch(String address) {
+    _addressRepository.searchAddress(address).then((data) {
+      _coordinates = LatLng(
+        data.latitude,
+        data.longitude,
+      );
+
+      setMapPosition(
+        data.formattedAddress,
+        data.longName,
+      );
+    }).catchError((onError) {
+      print(onError);
+    });
+  }
+
+  setMapPosition(title, snnipet) {
+    mapController.animateCamera(
+      CameraUpdate.newLatLng(
+        _coordinates,
+      ),
+    );
+
+    markers = <Marker>{};
+
+    final uuid = Uuid();
+    Marker marker = Marker(
+      markerId: MarkerId(uuid.v4()),
+      position: _coordinates,
+      infoWindow: InfoWindow(
+        title: title,
+        snippet: snnipet,
+      ),
+    );
+
+    markers?.add(marker);
+    setState(() {});
+  }
+
+  setCurrentLocation() async {
+    try {
+      await checkLocationPermission();
+
+      Position position = await Geolocator.getCurrentPosition(
+        desiredAccuracy: LocationAccuracy.high,
+      );
+
+      _coordinates = LatLng(
+        position.latitude,
+        position.longitude,
+      );
+
+      setMapPosition('Posição atual', 'Endereço não encontrado');
+    } catch (error) {
+      showDialog(
+          context: context,
+          builder: (_) {
+            return AlertDialog(
+              title: const Text('Erro'),
+              content: Text(error.toString()),
+              actions: [
+                TextButton(
+                    onPressed: () {
+                      Navigator.pop(context);
+                    },
+                    child: const Text('OK'))
+              ],
+            );
+          });
+    }
+  }
+
+  checkLocationPermission() async {
+    LocationPermission permission = await Geolocator.checkPermission();
+
+    if (permission == LocationPermission.denied) {
+      permission = await Geolocator.requestPermission();
+
+      if (permission == LocationPermission.deniedForever) {
+        throw 'Localização não está disponível';
+      }
+    }
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -26,16 +145,16 @@ class AddressView extends StatelessWidget {
               ),
               subtitle: Column(
                 crossAxisAlignment: CrossAxisAlignment.start,
-                children: const <Widget>[
+                children: <Widget>[
                   Text(
-                    "Rua do Desenvolvedor, 256",
-                    style: TextStyle(
+                    widget.contactModel.addressLine1!,
+                    style: const TextStyle(
                       fontSize: 12,
                     ),
                   ),
                   Text(
-                    "Piracicaba/SP",
-                    style: TextStyle(
+                    widget.contactModel.addressLine2!,
+                    style: const TextStyle(
                       fontSize: 12,
                     ),
                   ),
@@ -48,23 +167,35 @@ class AddressView extends StatelessWidget {
             height: 80,
             child: Padding(
               padding: const EdgeInsets.symmetric(horizontal: 20.0),
-              child: TextFormField(
+              child: TextField(
                 decoration: const InputDecoration(
                   labelText: "Pesquisar...",
                 ),
+                onSubmitted: (value) {
+                  onSearch(value);
+                },
               ),
             ),
           ),
           Expanded(
-            child: Container(
-              color: Colors.blue.withOpacity(0.2),
+            child: GoogleMap(
+              onMapCreated: _onMapCreated,
+              initialCameraPosition: CameraPosition(
+                target: _coordinates,
+                zoom: 11.0,
+              ),
+              markers: markers ?? <Marker>{},
             ),
           ),
         ],
       ),
       floatingActionButton: FloatingActionButton(
-        onPressed: () {},
-        child: const Icon(Icons.my_location),
+        onPressed: () {
+          setCurrentLocation();
+        },
+        child: const Icon(
+          Icons.my_location,
+        ),
       ),
     );
   }
